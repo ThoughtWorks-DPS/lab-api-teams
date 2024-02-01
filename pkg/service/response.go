3a17dd51b387358f372867aabbf8ae81baa776fa
Fuqiang Dong <fuqiang.dong01@gmail.com>
2024-02-01 13:50:50 -0600

    update.

--- pkg/service/response.go
+++ pkg/service/response.go
@@ -2,8 +2,12 @@ package service
 
 import "github.com/ThoughtWorks-DPS/lab-api-teams/pkg/domain"
 
-type ListNamespaceResponse struct {
-	Items      []domain.Namespace `json:"items"`
-	Page       int                `json:"page"`
-	MaxResults int                `json:"maxResults"`
+type ListResponse[K domain.Namespace | domain.Team] struct {
+	Items      []K `json:"items"`
+	Page       int `json:"page"`
+	MaxResults int `json:"maxResults"`
 }
+
+type ListNamespaceResponse ListResponse[domain.Namespace]
+
+type ListTeamResponse ListResponse[domain.Team]
